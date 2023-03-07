1c77fc7325bc1b6e134fa75d29f93fe57524aba9
kaspars <kaspars.mjaisans@gmail.com>
2023-03-07 16:33:26 +0200

    convert project and service slugs to styled-c

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,39 +1,40 @@
 import Image from 'next/image';
 import { TypeProject } from 'types';
 import { getStaticPaths, getStaticProps } from '../api/projectData';
-// import {
-//   getStaticPathsItem,
-//   getStaticPropsItem,
-// } from '../api/getStaticPropsAndPathsItem';
-
-// export const getStaticPaths = async () => {
-//   const { paths, fallback } = await getStaticPathsItem('project', null);
-//   return { paths, fallback };
-// };
-// export const getStaticProps = getStaticPropsItem('project');
+import styled from 'styled-components';
 
 const Project = ({ project }: { project: TypeProject }) => {
   const { content, coverImage, date, excerpt, slug, title } = project.fields;
 
   return (
-    <div className='project'>
-      <h2>{title}</h2>
-      <Image
+    <ProjectContainer>
+      <Title>{title}</Title>
+      <CoverImage
         src={`https:${coverImage.fields.file.url}`}
         alt='cover image'
         width={coverImage.fields.file.details.image!.width / 3}
         height={coverImage.fields.file.details.image!.height / 3}
       />
-      <style jsx>{`
-        .project {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-      `}</style>
-    </div>
+    </ProjectContainer>
   );
 };
 
 export { getStaticPaths, getStaticProps };
 export default Project;
+
+const ProjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const CoverImage = styled(Image)`
+  border-radius: 8px;
+  margin-bottom: 1rem;
+`;
--- src/pages/services/[slug].tsx
+++ src/pages/services/[slug].tsx
@@ -1,17 +1,15 @@
 import Image from 'next/image';
 import { TypeService } from 'types';
 import { getStaticPaths, getStaticProps } from '../api/serviceData';
-
-// import getContentTypeData from '../api/getContentType';
-// export const { getStaticPaths, getStaticProps } = getContentTypeData('service');
+import styled from 'styled-components';
 
 const Service = ({ service }: { service: TypeService }) => {
   const { coverImage, description, slug, title } = service.fields;
 
   return (
     <>
-      <h2>{title}</h2>
-      <Image
+      <Title>{title}</Title>
+      <CoverImage
         src={`https:${coverImage.fields.file.url}`}
         alt='cover image'
         width={coverImage.fields.file.details.image!.width / 3}
@@ -23,3 +21,14 @@ const Service = ({ service }: { service: TypeService }) => {
 
 export { getStaticPaths, getStaticProps };
 export default Service;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const CoverImage = styled(Image)`
+  border-radius: 8px;
+  margin: 1rem 0;
+`;
