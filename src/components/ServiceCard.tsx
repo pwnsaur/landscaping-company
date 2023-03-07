26b4d8010010bc415f5f5b3c1f04e3c202c68fc9
kaspars <kaspars.mjaisans@gmail.com>
2023-03-07 16:18:55 +0200

    convert ServiceCard to styled-c

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,33 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { TypeService } from 'types';
+import styled from 'styled-components';
 
 const Service = ({ service }: { service: TypeService }) => {
   const { coverImage, slug, title, description } = service.fields;
 
   return (
-    <div className='card'>
+    <Card>
       <Link href={`/services/${slug}`}>
-        <div className='title'>{title}</div>
+        <Title>{title}</Title>
         <Image
-          className='image'
           src={`https:${coverImage.fields.file.url}`}
           alt='cover-image'
           height={350}
           width={500}
         />
       </Link>
-
-      <style jsx>{`
-        .card {
-          display: flex;
-          justify-content: center;
-          padding: 20px;
-          text-align: center;
-        }
-      `}</style>
-    </div>
+    </Card>
   );
 };
 
 export default Service;
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+`;
