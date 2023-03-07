63b40d324e05b14dd7efe7f2f2a9b14d133cf7db
kaspars <kaspars.mjaisans@gmail.com>
2023-03-07 16:40:14 +0200

    conver 404 page to styled-c

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,23 @@
+import styled from 'styled-components';
+
 const NotFound = () => {
   return (
-    <div className='not-found'>
-      <h1>404</h1>
-
-      <style jsx>{`
-        .not-found {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          height: 400px;
-        }
-      `}</style>
-    </div>
+    <NotFoundContainer>
+      <Title>404</Title>
+    </NotFoundContainer>
   );
 };
 
 export default NotFound;
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+`;
