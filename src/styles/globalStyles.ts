a99e93405c7040d6107d1cede30b991dfa3fc82b
kaspars <kaspars.mjaisans@gmail.com>
2023-03-08 15:26:54 +0200

    switch global styled to styled-c

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['latin', 'latin-ext'],
+  display: 'swap',
+  fallback: ['sans-serif'],
+});
+
+const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: ${nunito.style.fontFamily}, sans-serif;
+}
+
+.container {
+  width: 100%;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+`;
+
+export default GlobalStyles;
