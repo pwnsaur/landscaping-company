1532606fccf1293b8462c8c21ca05e06afe539a0
kaspars <kaspars.mjaisans@gmail.com>
2023-03-15 12:28:32 +0200

    make some progress with the parallax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import { NextSeo } from 'next-seo';
 import { Parallax } from 'react-scroll-parallax';
 import Image from 'next/image';
-import backgroundImage from '../../public/backgroundImage.jpg';
+import backgroundImageOne from '../../public/backgroundImageOne.jpg';
+import backgroundImageTwo from '../../public/backgroundImageTwo.jpg';
 
 const Home = () => {
   return (
@@ -15,11 +16,48 @@ const Home = () => {
 
       <Main>
         <Parallax translateY={[-150, 220]}>
-          <BackgroundImage src={backgroundImage} alt='background' priority />
+          <Wrapper>
+            <BackgroundImage
+              src={backgroundImageOne}
+              alt='background'
+              priority
+            />
+          </Wrapper>
+
+          <Wrapper>
+            <BackgroundImage
+              src={backgroundImageTwo}
+              alt='background'
+              priority
+            />
+          </Wrapper>
         </Parallax>
-        <Content>
-          <Title>Sākums</Title>
-        </Content>
+
+        <SectionsGrid>
+          <Section>
+            <TransparentContainer />
+            <SectionContent>
+              <Title>Section 1</Title>
+              <p>Something, something</p>
+            </SectionContent>
+          </Section>
+
+          <Section>
+            <TransparentContainer />
+            <SectionContent>
+              <Title>Section 2</Title>
+              <p>Something, something</p>
+            </SectionContent>
+          </Section>
+
+          <Section>
+            <TransparentContainer />
+            <SectionContent>
+              <Title>Section 3</Title>
+              <p>Something, something</p>
+            </SectionContent>
+          </Section>
+        </SectionsGrid>
       </Main>
     </>
   );
@@ -30,11 +68,12 @@ export default Home;
 const Main = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
-  padding: 6rem;
-  height: 100%;
   width: 100%;
+  position: relative;
+  overflow: hidden;
+  margin-top: 2rem;
+  padding-bottom: 55vh;
 `;
 
 const BackgroundImage = styled(Image)`
@@ -44,15 +83,46 @@ const BackgroundImage = styled(Image)`
   z-index: -1;
 `;
 
-const Content = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
-`;
-
 const Title = styled.h2`
   color: #fff;
   font-size: 3rem;
+`;
+
+const SectionsGrid = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 100vh);
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+const TransparentContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  z-index: 1;
+`;
+
+const SectionContent = styled.div`
+  position: relative;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  width: calc(100% - 2rem);
+  margin: auto;
+  overflow: hidden;
+  position: relative;
 `;
