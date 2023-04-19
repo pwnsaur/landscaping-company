845bdedffa5f947496a8875aa26ddf35c8f64685
kaspars <kaspars.mjaisans@gmail.com>
2023-04-19 12:18:07 +0300

    improve button readability

--- src/components/reusables/SquareButton.tsx
+++ src/components/reusables/SquareButton.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Link from 'next/link';
 
 interface SqareButtonProps {
@@ -17,29 +17,56 @@ const SqareButton = ({ name, destination, isInverted }: SqareButtonProps) => {
 
 export default SqareButton;
 
+// const Button = styled.button<{ $inverted?: boolean }>`
+//   min-width: 6rem;
+//   height: 3rem;
+//   padding: 0 1rem;
+//   background-color: ${({ theme, $inverted }) =>
+//     $inverted ? theme.colors.darkGreen : theme.colors.background};
+//   border: 2px solid
+//     ${({ theme, $inverted }) =>
+//       $inverted ? theme.colors.background : theme.colors.darkGreen};
+//   color: ${({ theme, $inverted }) =>
+//     $inverted ? theme.colors.background : theme.colors.darkGreen};
+//   text-transform: uppercase;
+//   font-weight: 600;
+//   font-size: 1rem;
+//   cursor: pointer;
+//   transition: all 0.2s ease-in-out;
+
+//   &:hover {
+//     background-color: ${({ theme, $inverted }) =>
+//       $inverted ? theme.colors.background : theme.colors.darkGreen};
+//     border-color: ${({ theme, $inverted }) =>
+//       $inverted ? theme.colors.darkGreen : theme.colors.background};
+//     color: ${({ theme, $inverted }) =>
+//       $inverted ? theme.colors.darkGreen : theme.colors.background};
+//   }
+// `;
+
 const Button = styled.button<{ $inverted?: boolean }>`
   min-width: 6rem;
   height: 3rem;
   padding: 0 1rem;
-  background-color: ${({ theme, $inverted }) =>
-    $inverted ? theme.colors.darkGreen : theme.colors.background};
-  border: 2px solid
-    ${({ theme, $inverted }) =>
-      $inverted ? theme.colors.background : theme.colors.darkGreen};
-  color: ${({ theme, $inverted }) =>
-    $inverted ? theme.colors.background : theme.colors.darkGreen};
   text-transform: uppercase;
   font-weight: 600;
   font-size: 1rem;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
 
-  &:hover {
-    background-color: ${({ theme, $inverted }) =>
-      $inverted ? theme.colors.background : theme.colors.darkGreen};
-    border-color: ${({ theme, $inverted }) =>
-      $inverted ? theme.colors.darkGreen : theme.colors.background};
-    color: ${({ theme, $inverted }) =>
-      $inverted ? theme.colors.darkGreen : theme.colors.background};
-  }
+  ${({ theme, $inverted }) => {
+    const { background, darkGreen } = theme.colors;
+
+    return css`
+      background-color: ${$inverted ? darkGreen : background};
+      border: 2px solid ${$inverted ? background : darkGreen};
+      color: ${$inverted ? background : darkGreen};
+
+      &:hover {
+        background-color: ${$inverted ? background : darkGreen};
+        border-color: ${$inverted ? darkGreen : background};
+        color: ${$inverted ? darkGreen : background};
+      }
+    `;
+  }}
 `;
