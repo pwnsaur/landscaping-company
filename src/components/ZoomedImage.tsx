aee4deb0ea844755af5a07cc66c8f640e8b72e42
kaspars <kaspars.mjaisans@gmail.com>
2023-06-06 13:03:59 +0300

    add naivigation through zoomed images

--- src/components/ZoomedImage.tsx
+++ src/components/ZoomedImage.tsx
@@ -1,4 +1,6 @@
+import { Asset } from 'contentful';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 interface ZoomedImageProps {
@@ -7,16 +9,56 @@ interface ZoomedImageProps {
   width: number;
   height: number;
   close: () => void;
+  previous: () => void;
+  next: () => void;
+  zoomedImageIndex: number;
+  images?: Asset[];
 }
 
-const ZoomedImage = ({ src, alt, width, height, close }: ZoomedImageProps) => {
+const ZoomedImage = ({
+  src,
+  alt,
+  width,
+  height,
+  close,
+  previous,
+  next,
+  zoomedImageIndex,
+  images = [],
+}: ZoomedImageProps) => {
+  const [leftDisabled, setLeftDisabled] = useState(false);
+  const [rightDisabled, setRightDisabled] = useState(false);
+
+  useEffect(() => {
+    setLeftDisabled(zoomedImageIndex === 0);
+    setRightDisabled(zoomedImageIndex === images.length - 1);
+  }, [zoomedImageIndex, images]);
+
   const aspectRatio = height / width;
 
   return (
     <Overlay onClick={close}>
+      <ArrowContainerLeft
+        onClick={(e) => {
+          e.stopPropagation();
+          previous();
+        }}
+        disabled={leftDisabled}
+      >
+        <Arrow disabled={leftDisabled}>{'<'}</Arrow>
+      </ArrowContainerLeft>
       <ImageWrapper $aspectRatio={aspectRatio}>
         <StyledImage src={src} alt={alt} width={width} height={height} />
       </ImageWrapper>
+      <ArrowContainerRight
+        onClick={(e) => {
+          e.stopPropagation();
+          next();
+        }}
+        disabled={rightDisabled}
+      >
+        <Arrow disabled={rightDisabled}>{'>'}</Arrow>
+      </ArrowContainerRight>
     </Overlay>
   );
 };
@@ -24,15 +66,19 @@ const ZoomedImage = ({ src, alt, width, height, close }: ZoomedImageProps) => {
 export default ZoomedImage;
 
 const StyledImage = styled(Image)`
+  object-fit: contain;
   width: 100%;
-  height: auto;
+  height: 100%;
 `;
 
 const ImageWrapper = styled.div<{ $aspectRatio: number }>`
-  position: relative;
-  max-width: 80%;
-  max-height: 80%;
-  padding-bottom: calc(80% * ${({ $aspectRatio }) => $aspectRatio});
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% - 140px);
+  height: auto;
+  max-height: 100%;
+  aspect-ratio: ${({ $aspectRatio }) => $aspectRatio};
 `;
 
 const Overlay = styled.div`
@@ -41,10 +87,58 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.8);
+  background: rgba(255, 255, 255, 0.8);
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 5rem;
+  padding: 1rem;
   z-index: 6;
+`;
+
+const Arrow = styled.button`
+  position: absolute;
+  z-index: 7;
+  top: 50%;
+  color: ${({ theme }) => theme.colors.black};
+  background: transparent;
+  border: none;
+  font-size: ${({ theme }) => theme.fontSizes.superLarge};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  transform: translateY(-50%);
+  cursor: pointer;
+
+  &[disabled] {
+    color: ${({ theme }) => theme.colors.borderGrey};
+    cursor: default;
+  }
+`;
+
+const ArrowContainer = styled.div<{ disabled: boolean }>`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 70px;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+
+  &:not([disabled]):hover {
+    background: rgba(200, 200, 200, 0.6);
+  }
+
+  &:not([disabled]) {
+    background: rgba(200, 200, 200, 0.3);
+  }
+
+  &[disabled] {
+    cursor: default;
+  }
+`;
+
+const ArrowContainerLeft = styled(ArrowContainer)`
+  left: 0;
+`;
+
+const ArrowContainerRight = styled(ArrowContainer)`
+  right: 0;
 `;
