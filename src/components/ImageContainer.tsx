5c5719cb417165f8f639904e4061c6c647a3cb54
Kaspars <kaspars.mjaisans@gmail.com>
2023-06-06 16:01:50 +0300

    add naivigation through zoomed images (#12)
    * add naivigation through zoomed images
    
    * improve passed props

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,6 +1,6 @@
 import { Asset } from 'contentful';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styled from 'styled-components';
 
 import useIsMobile from '@/utils/hooks/useIsMobile';
@@ -11,58 +11,81 @@ type Props = {
 };
 
 const ImageContainer = ({ images }: Props) => {
-  const [zoomedImage, setZoomedImage] = useState<{
-    src: string;
-    width: number;
-    height: number;
-  } | null>(null);
-
+  const [zoomedImageIndex, setZoomedImageIndex] = useState<number | null>(null);
   const isMobile = useIsMobile();
 
-  const handleClick = (src: string, width: number, height: number) => {
-    if (isMobile) return;
-    setZoomedImage({ src, width, height });
-    document.body.style.overflow = 'hidden';
+  const handleClick = (index: number) => {
+    if (!isMobile) {
+      setZoomedImageIndex(index);
+      document.body.style.overflow = 'hidden';
+    }
   };
 
-  const handleClose = () => {
-    if (isMobile) return;
-    setZoomedImage(null);
+  const handleClose = useCallback(() => {
+    setZoomedImageIndex(null);
     document.body.style.overflow = 'auto';
-  };
+  }, [setZoomedImageIndex]);
+
+  const navigateImage = useCallback(
+    (direction: number) => {
+      if (!images || zoomedImageIndex === null) {
+        return;
+      }
+
+      const newIndex = zoomedImageIndex + direction;
+      if (newIndex >= 0 && newIndex < images.length) {
+        setZoomedImageIndex(newIndex);
+      }
+    },
+    [images, zoomedImageIndex]
+  );
+
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        navigateImage(-1);
+      } else if (e.key === 'ArrowRight') {
+        navigateImage(1);
+      } else if (e.key === 'Escape') {
+        handleClose();
+      }
+    };
+
+    if (zoomedImageIndex !== null) {
+      window.addEventListener('keydown', handleKeydown);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [zoomedImageIndex, navigateImage, handleClose]);
 
   return (
     <>
       <Container>
-        {images &&
-          images.map((image) => (
-            <ImageWrapper
-              key={image.sys.id}
-              onClick={() =>
-                handleClick(
-                  `https:${image.fields.file.url}`,
-                  image.fields.file.details.image!.width,
-                  image.fields.file.details.image!.height
-                )
-              }
-            >
-              <StyledImage
-                src={`https:${image.fields.file.url}`}
-                alt='project image'
-                height={image.fields.file.details.image!.height / 4}
-                width={image.fields.file.details.image!.width / 4}
-                quality={50}
-              />
-            </ImageWrapper>
-          ))}
+        {images?.map((image, index) => (
+          <ImageWrapper key={image.sys.id} onClick={() => handleClick(index)}>
+            <StyledImage
+              src={`https:${image.fields.file.url}`}
+              alt='project image'
+              height={image.fields.file.details.image!.height / 4}
+              width={image.fields.file.details.image!.width / 4}
+              quality={50}
+            />
+          </ImageWrapper>
+        ))}
       </Container>
-      {zoomedImage && !isMobile && (
+      {zoomedImageIndex !== null && !isMobile && images && (
         <ZoomedImage
-          src={zoomedImage.src}
+          src={`https:${images[zoomedImageIndex].fields.file.url}`}
           alt='zoomed image'
-          width={zoomedImage.width}
-          height={zoomedImage.height}
+          width={images[zoomedImageIndex].fields.file.details.image!.width}
+          height={images[zoomedImageIndex].fields.file.details.image!.height}
           close={handleClose}
+          previous={() => navigateImage(-1)}
+          next={() => navigateImage(1)}
+          zoomedImageIndex={zoomedImageIndex}
+          imagesLength={images.length}
         />
       )}
     </>
